c84e67ef120cbc35e5d32de26b80d0c8106d1d83
Ross Gonsolvis <gonsolvis@gmail.com>
2023-02-27 16:04:28 +0100

    updated

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './App.css';
-import icon1 from './images/icon1.png';
-import icon2 from './images/icon2.png';
-import icon3 from './images/icon3.png';
-import icon4 from './images/icon4.png';
-import ironhackLogo from './images/ironhack-logo-xs.png';
-import menuTop from './images/menu-top-xs.png';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
